dafd0ff765a797e7024b86cf0327c4fd72351391
rishabhs <rishi@billte.ch>
2025-12-17 20:58:50 +0530

    fixed code

--- coreproject/security/src/main/java/com/workspark/security/model/AuthUser.java
+++ coreproject/security/src/main/java/com/workspark/security/model/AuthUser.java
@@ -1,24 +1,24 @@
 package com.workspark.security.model;
 
 import com.workspark.models.enums.UserRoleEnum;
+import jdk.jfr.DataAmount;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AuthUser implements UserDetails {
 
-    private Long id;
+    private String id;
     private String firstName;
     private String lastName;
     private String phoneNumber;
@@ -28,19 +28,36 @@ public class AuthUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles.stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
-                .toList();
+        return List.of();
     }
 
     @Override
     public String getPassword() {
-        return password;
+        return "";
     }
 
     @Override
     public String getUsername() {
-        return email;
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 }
-
